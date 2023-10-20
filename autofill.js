c7d5452a92e832bb5570bcc1b4e35a138c1d5a27
adinxu <1334585420@qq.com>
2023-10-20 20:35:28 +0800

    add xpath query method

--- autofill.js
+++ autofill.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 });
 function click_and_wait(){
     let ele_start_btn = "#__layout > div > div > div > div.reserve-msg > div.reserve-button > div:nth-child(1)";
-    let start_btn = document.querySelector(ele_start_btn);
+    let start_btn = queryByCCS(ele_start_btn);
     if(start_btn){
         start_btn.click();
     }else{
@@ -35,7 +35,7 @@ function wait_and_go(){
 function refresh_and_enter() {
     //first stage
     let ele_enter_btn = "#__layout > div > div > div.confirm-btn.disable";
-    let enter_btn = document.querySelector(ele_enter_btn);
+    let enter_btn = queryByCCS(ele_enter_btn);
     if(enter_btn){
         console.log("first stage process===================================================");
         let in_text = enter_btn.innerText;
@@ -56,7 +56,7 @@ function refresh_and_enter() {
 function fill_info_and_submit() {
     //second stage
     let ele_add_person_btn = "#__layout > div > div > div.person-box > div.person-msg > div > div.add-person"
-    let add_person_btn = document.querySelector(ele_add_person_btn);
+    let add_person_btn = queryByCCS(ele_add_person_btn);
 
     if(add_person_btn && add_person_btn.innerText == "添加参观者"){
         console.log("second stage process===================================================");
@@ -72,9 +72,9 @@ function fill_info_and_submit_real(){
     let ele_name = "#__layout > div > div > div.person-box > div.person-msg > div > div.viewer-form-box > div > div.view_form_container > div:nth-child(1) > input";
     let ele_num = "#__layout > div > div > div.person-box > div.person-msg > div > div.viewer-form-box > div > div.view_form_container > div:nth-child(3) > input";
     let ele_submit_btn = "#__layout > div > div > div.person-box > div.person-btn > div";
-    let name = document.querySelector(ele_name);
-    let num = document.querySelector(ele_num);
-    let submit_btn = document.querySelector(ele_submit_btn);
+    let name = queryByCCS(ele_name);
+    let num = queryByCCS(ele_num);
+    let submit_btn = queryByCCS(ele_submit_btn);
     if(name && num && submit_btn){
         try_to_fill_input_use_event(name, "张三");
         try_to_fill_input_use_event(num, "370728195508183628");
@@ -96,7 +96,7 @@ async function check_wait_state(tval) {
 
     console.log("等待结束");
     let ele_wait_btn = "#__layout > div > div > div > div > div.button > div"
-    let wait_btn = document.querySelector(ele_wait_btn);
+    let wait_btn = queryByCCS(ele_wait_btn);
     if(wait_btn && wait_btn.innerText == "我已阅读并同意声明内容"){
         console.log(wait_btn.innerText)
         wait_btn.click()
@@ -117,4 +117,11 @@ function try_to_fill_input_use_focus(input, str_to_fill){
     input.focus();
     document.execCommand('inserttext',false,str_to_fill);
     input.blur(); //失去焦点
+}
+
+function queryByXPATH(xpath_str){
+    return document.evaluate(xpath_str, document, null, XPathResult.ANY_TYPE, null).iterateNext();
+}
+function queryByCCS(ccs_str){
+    return document.querySelector(ccs_str);
 }
